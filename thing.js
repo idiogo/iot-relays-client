8619573f4d7db0e400ed99cf4667e59545497cf2
Diogo Carneiro <contato@idiogo.com.br>
2018-02-09 23:47:17 -0200

    gpio

--- thing.js
+++ thing.js
@@ -1,26 +1,47 @@
 'use strict'
 
-const gpio_set = (gpio,value) => {
-    console.log(gpio+'='+value)
+var GPIO = require('rpi-gpio');
+ 
+const gpio_set = (gpio,value,callback) => {
+    GPIO.write(17, true, function(err) {
+        if (err) throw err;
+        console.log(gpio+'='+value)
+        callback()
+    });    
 }
 
-module.exports.trigger = (gpio, callback) => {
-    gpio_set(gpio,'LOW')
-    setTimeout(() => {
-        gpio_set(gpio,'HIGH')
-        setTimeout(() => {
-            gpio_set(gpio,'LOW')
+module.exports.trigger = (thing, callback) => {
+    const gpio = 17
+    GPIO.setup(gpio, gpio.DIR_OUT, ()=>{
+        gpio_set(gpio,'LOW',()=>{
+            setTimeout(() => {
+                gpio_set(gpio,'HIGH',()=>{
+                    setTimeout(() => {
+                        gpio_set(gpio,'LOW',()=>{
+                            callback(true)
+                        })
+                    },400)
+                })
+            },400)
+        })
+    })
+    
+}
+
+module.exports.on = (thing, callback) => {
+    const gpio = 17
+    GPIO.setup(gpio, gpio.DIR_OUT, ()=>{
+        gpio_set(gpio,'HIGH',()=>{
             callback(true)
-        },400)
-    },400)
+        })
+    })
 }
 
-module.exports.on = (gpio, callback) => {
-    gpio_set(gpio,'HIGH')
-    callback(true)
-}
-
-module.exports.off = (gpio, callback) => {
-    gpio_set(gpio,'LOW')
-    callback(true)
+module.exports.off = (thing, callback) => {
+    const gpio = 17
+    GPIO.setup(gpio, gpio.DIR_OUT, ()=>{
+        gpio_set(gpio,'LOW',()=>{
+            callback(true)
+        })
+    })
 }
