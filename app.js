f9855f621efc11488b10f18f0cdd8f7ad31555b4
Diogo Carneiro <contato@idiogo.com.br>
2018-02-13 00:31:59 -0200

    ambiente de dev e tratamento de erros

--- app.js
+++ app.js
@@ -3,27 +3,37 @@
 const io = require('socket.io-client');
 const Thing = require('./thing')
 const socket = io(process.env.SOCKET);
+const setup = JSON.parse(process.env.SETUP) //TODO: verify undefined
+const things = Thing.parseThings(setup.things)
 
 socket.on('connect', function(data) {
     socket.emit('register', process.env.SETUP)
 })
 
 socket.on('hardware-trigger', function(data) {
-    Thing.trigger(data.thing, (success) => {
+    Thing.trigger(things[data.thing], (success) => {
         socket.emit('callback-'+data.requestID, success)
     })
 })
 
 socket.on('hardware-on', function(data) {
-    Thing.on(data.thing, (success) => {
+    Thing.on(things[data.thing], (success) => {
         socket.emit('callback-'+data.requestID, success)
     })
 })
 
+socket.on('ui-loaded', function(data) {
+    console.log(data);
+})
+
 socket.on('hardware-off', function(data) {
-    Thing.off(data.thing, (success) => {
+    Thing.off(things[data.thing], (success) => {
         socket.emit('callback-'+data.requestID, success)
     })
+})
+
+socket.on('error', function(error) {
+    console.log(error)
 })
 
 //SETUP={\"name\":\"linden-gardens\",\"things\":[{\"name\":\"portaria-externa\",\"port_numer\":\"19\"},{\"name\":\"portaria-interna\",\"port_numer\":\"17\"},{\"name\":\"garagem-terreo\",\"port_numer\":\"18\"},{\"name\":\"garagem-subsolo\",\"port_numer\":\"20\"}]} nodemon app.js 
--- thing.js
+++ thing.js
@@ -1,21 +1,17 @@
 'use strict'
 
 var GPIO = require('gpio');
- 
-const gpio_set = (gpio_number,value,callback) => {
-    var gpio = GPIO.export(gpio_number, {
-       direction: "out",
-       ready: function() {
-           gpio.set(value, function() {
-               console.log(gpio.value);
-               callback()
-           });
-       }
-    });   
+
+module.exports.parseThings = (things) => {
+    var thingsDict = {}
+    things.forEach((thing) => {
+        thingsDict[thing.name] = thing
+    })
+    return thingsDict
 }
 
 module.exports.trigger = (thing, callback) => {
-    const gpio_number = 17
+    const gpio_number = thing.port_number
     gpio_set(gpio_number,0,()=>{
         setTimeout(() => {
             gpio_set(gpio_number,1,()=>{
@@ -30,15 +26,31 @@ module.exports.trigger = (thing, callback) => {
 }
 
 module.exports.on = (thing, callback) => {
-    const gpio_number = 17
+    const gpio_number = thing.port_number
     gpio_set(gpio_number,1,()=>{
         callback(true)
     })
 }
 
 module.exports.off = (thing, callback) => {
-    const gpio_number = 17
+    const gpio_number = thing.port_number
     gpio_set(gpio_number,0,()=>{
         callback(true)
     })
+}
+
+const gpio_set = (gpio_number,value,callback) => {
+    if (typeof GPIO.exists !== 'undefined') {
+        var gpio = GPIO.export(gpio_number, {
+           direction: "out",
+           ready: function() {
+               gpio.set(value, function() {
+                   callback()
+               });
+           }
+        });   
+    }else{ //dev
+        console.log(gpio_number, value);
+        callback()
+    }
 }
